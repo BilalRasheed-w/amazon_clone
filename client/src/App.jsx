36e2f5d4def6a05f2477ef6c2d139ffd735ff955
bilal <bilalrasheedw@gmail.com>
2023-08-07 18:48:56 +0530

    app drawer and menu completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <Navbar />
       <BreadCrumb />
       <Routes>
+      <Route path="/" element={<Hero />} />
       </Routes>
     </Router>
   );
---
+++ client/src/screens/Hero.jsx
@@ -0,0 +1,14 @@
+import { HStack, Heading, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <Stack w={"full"} height={"35vh"} bg={"cyan.200"}  justifyContent={'center'} alignItems={'center'} >
+        <Heading>hello iam carousel section</Heading>
+      </Stack>
+    </>
+  );
+};
+
+export default Hero;
